629e53182183f061d9b1d71d3ad2d87a71325306
nolan <nolan.benoit49@gmail.com>
2026-06-23 10:49:12 +0200

    pzrtie.c

--- partie.c
+++ partie.c
@@ -34,13 +34,13 @@ void distribuer_carte_depart(Partie *partie)
         while (c<TAILLE_MAX_MAIN || partie->joueur[j].nb_cartes<9) //tant que le joueur a pas 9 cartes
         {
             int position;
-            while(1) //boucle infinie pour vérifier que la carte n'est pas deja utilisée
+            while(1) //boucle infinie pour vÃ©rifier que la carte n'est pas deja utilisÃ©e
             {
                 position=rand()% NB_CARTES_MAX;
                 if (partie->paquet.carte[position].utilisee_tri==0)
                     break;
             }
-            partie->paquet.carte[position].utilisee_tri=1;     //marque la carte tirée
+            partie->paquet.carte[position].utilisee_tri=1;     //marque la carte tirÃ©e
             partie->joueur[j].main[c]=partie->paquet.carte[position];  //donne la carte au joueur
             partie->joueur[j].nb_cartes++;
             c++;
@@ -50,38 +50,73 @@ void distribuer_carte_depart(Partie *partie)
     }
 }
 
-void demande_carte_a_jouer(Partie *partie)
+void demande_carte_a_jouer(Partie *partie, int id_joueur)
 {
     int saisie_valide=0;
     Pile_milieu pile;
     pile.nb_carte_milieu=0;
+    int centre = partie->pile_milieu.nb_carte_milieu;
     int i;
     while (1)
     {
          positionner_curseur(120,0);
          printf("combien de cartes veux tu jouer ? (si tu es le 1er a jouer, joue une seule carte)");
-         scanf("%d",&partie->joueur->nb_cartes_jouees);
-         if (partie->joueur->nb_cartes_jouees>=0 && partie->joueur->nb_cartes_jouees<=TAILLE_MAX_MAIN && partie->joueur->nb_cartes_jouees>=pile.nb_carte_milieu && partie->joueur->nb_cartes_jouees<=pile.nb_carte_milieu+1)
-            {
-               break;
-            }
+         if(scanf("%d", &partie->joueur[id_joueur].nb_cartes_jouees) != 1) {
+            while(getchar() != '\n');
 
+         if (partie->joueur[id_joueur].nb_cartes_jouees >= 0 && partie->joueur[id_joueur].nb_cartes_jouees <= partie->joueur[id_joueur].nb_cartes && partie->joueur[id_joueur].nb_cartes_jouees >= centre && partie->joueur[id_joueur].nb_cartes_jouees <= centre + 1)
+         {
+             break;}
     }
     while (saisie_valide==0)
     {
-        for (i=0; i<partie->joueur->nb_cartes_jouees; i++)
+        int compteur_bonnes_cartes= 0;
+        for (i=0; i<partie->joueur[id_joueur].nb_cartes_jouees; i++)
         {
             printf("carte numero %d ?",i);  //demande quelles cartes ?
-            scanf("%d",&partie->joueur->choix[i]);
 
-            if (partie->joueur->choix[i]-1>=0 && partie->joueur->choix[i]-1<=partie->joueur->nb_cartes) //verif que la saisie est bonne
-            {
-               saisie_valide=1;
+            if(scanf("%d", &partie->joueur[id_joueur].choix[i]) != 1) {
+            while(getchar() != '\n');
             }
-        }
+
+            if (partie->joueur[id_joueur].choix[i]-1 >= 0 &&partie->joueur[id_joueur].choix[i]-1 < partie->joueur[id_joueur].nb_cartes)
+            {
+               compteur_bonnes_cartes++; //verif que la saisie est bonne
+
+            if (compteur_bonnes_cartes== partie->joueur[id_joueur].nb_cartes_jouees)        {
+            saisie_valide =  1;
+
+            }
+    }
     }
 }
+int verifier_combinaison_valide(Partie *partie, int id_joueur)
+{
+    int nb_jouees = partie->joueur[id_joueur].nb_cartes_jouees;
+    
+    if (nb_jouees <= 1) return 1; 
 
+    int meme_couleur = 1; 
+    int meme_chiffre = 1; 
+
+    int idx_ref = partie->joueur[id_joueur].choix[0] - 1;
+    int couleur_ref = partie->joueur[id_joueur].main[idx_ref].couleur;
+    int chiffre_ref = partie->joueur[id_joueur].main[idx_ref].chiffre;
+
+   
+    for(int i = 1; i < nb_jouees; i++) 
+    {
+        int idx_carte =partie->joueur[id_joueur].choix[i] - 1;
+        
+        if (partie->joueur[id_joueur].main[idx_carte].couleur != couleur_ref) 
+            
+            meme_couleur = 0;
+        if (partie->joueur[id_joueur].main[idx_carte].chiffre!= chiffre_ref) 
+            meme_chiffre = 0;
+    }
+
+    return (meme_couleur == 1 || meme_chiffre == 1); // renvoie 1 si au moins 1 des 2 vrai
+}
 void jouer_cartes( Partie *partie)
 {
     int choix_carte_recuperee;
@@ -92,7 +127,7 @@ void jouer_cartes( Partie *partie)
         partie->joueur->main[TAILLE_MAX_MAIN+1]=partie->pile_milieu.carte_posee[choix_carte_recuperee];
     }
     //--------------------------------------------------
-    int a_joue[TAILLE_MAX_MAIN]={0};  //utile pour faire la nouvelle main après avoir jouer
+    int a_joue[TAILLE_MAX_MAIN]={0};  //utile pour faire la nouvelle main aprÃ¨s avoir jouer
     for (int i=0; i<partie->joueur->nb_cartes_jouees; i++)
     {
         a_joue[partie->joueur->choix[i]-1]=1;
@@ -104,7 +139,7 @@ void jouer_cartes( Partie *partie)
      int nouvelle_taille_main=0;
      for (int j = 0; j < partie->joueur->nb_cartes; j++)
     {
-        if(a_joue[j]==0)    //si la carte est pas jouée alors... si elle est jouée, pas d'action donc nouvelle taille main se decale de j, donc réordonne la main
+        if(a_joue[j]==0)    //si la carte est pas jouÃ©e alors... si elle est jouÃ©e, pas d'action donc nouvelle taille main se decale de j, donc rÃ©ordonne la main
         {
             partie->joueur->main[nouvelle_taille_main]=partie->joueur->main[j];
             nouvelle_taille_main++;
@@ -161,7 +196,7 @@ void calcul_num_pose(Partie partie)
 }
 
 //------------------------------------------------------------------------------------------------
-//-------------------------menu d'entrée
+//-------------------------menu d'entrÃ©e
 void menu_complet()
 {
     plein_ecran();
@@ -197,7 +232,9 @@ void demander_prenom()
 }
 
 void tour_de_jeu(Partie *partie)
+
 {
+
     int ecartement=0;
     int j;
     int c;
@@ -209,13 +246,27 @@ void tour_de_jeu(Partie *partie)
         for ( j=0; j< partie->nb_joueur; j++)
         {
             for (c=0; c<9; c++) //ecartement des cartes
-            {
 
+            {
                 dessiner_carte_du_jeu( 34, ecartement, partie->joueur[j].main[c].couleur, partie->joueur[j].main[c].chiffre);
                 ecartement=ecartement+PAS_ENTRE_CARTES;
-            }
-            demande_carte_a_jouer(partie);
-            jouer_cartes(partie);
+                }
+            int combinaison_valide = 0; // part du principe que c'est pas bon
+            do
+            {
+                demande_carte_a_jouer(partie, j); // j pour cibler les joueurs jusqu'a nb joueur
+                if (verifier_combinaison_valide(partie, j) == 1)
+                {
+                    combinaison_valide = 1;
+                        }
+                    else
+                    {
+                    printf("\nERREUR : Les cartes choisies doivent etre meme couleur ou chiffre !\n");
+                    system("PAUSE"); //
+                }
+            } while (combinaison_valide == 0); // le do whhile si c'est faux, on affiche une erreur et la boucle va recommencer
+            jouer_cartes(partie, j);
+
         }
 
-}
+    
